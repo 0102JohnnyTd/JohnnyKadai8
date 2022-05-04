0c1b680460eb9d1b4a17f6ef6b0d46d2cc15b397
0102johnny <rt2824.recruit.1022@gmail.com>
2022-05-04 09:58:42 +0900

    両画面のクラスファイルを追加

---
+++ JohnnyKadai8/AViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AViewController.swift
+//  JohnnyKadai8
+//
+//  Created by Johnny Toda on 2022/05/04.
+//
+
+import UIKit
+
+class AViewController: UIViewController {
+
+    @IBOutlet private weak var valueLabelA: UILabel!
+
+    @IBOutlet private weak var sliderA: UISlider!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
