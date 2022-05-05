8bac888fd0228f9366388a2dd4a9d7fa6ab314b8
0102johnny <rt2824.recruit.1022@gmail.com>
2022-05-05 18:26:14 +0900

    delegateを使って画面Aのsliderの値を画面Bのsliderの値に渡す処理を追加した

--- JohnnyKadai8/AViewController.swift
+++ JohnnyKadai8/AViewController.swift
@@ -7,12 +7,28 @@
 
 import UIKit
 
+protocol CustomSliderDelegate {
+    func passToSliderValue(slider: UISlider)
+}
+
 final class AViewController: UIViewController {
     @IBOutlet private weak var valueLabelA: UILabel!
 
     @IBOutlet private weak var sliderA: UISlider!
 
+    @IBAction func changeSliderValue(_ sender: Any) {
+        valueLabelA.text = "\(sliderA.value)"
+        delegate?.passToSliderValue(slider: sliderA)
+    }
+
+    var delegate: CustomSliderDelegate?
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        getSliderValue()
+    }
+
+    private func getSliderValue() {
+        valueLabelA.text = "\(sliderA.value)"
     }
 }
--- JohnnyKadai8/BViewController.swift
+++ JohnnyKadai8/BViewController.swift
@@ -9,9 +9,23 @@ import UIKit
 
 final class BViewController: UIViewController {
     @IBOutlet private weak var valueLabelB: UILabel!
+
     @IBOutlet private weak var sliderB: UISlider!
+
+    @IBAction private func getSliderValue(_ sender: Any) {
+        valueLabelB.text = "\(sliderB.value)"
+    }
+
+    private let viewControllerA = UIStoryboard(name: "A", bundle: nil).instantiateViewController(withIdentifier: "A") as! AViewController
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        viewControllerA.delegate = self
+    }
+}
+
+extension BViewController: CustomSliderDelegate {
+    func passToSliderValue(slider: UISlider) {
+        sliderB.value = slider.value
     }
 }
